0543366a1b2a03364955b7f384164914fed95ff4
Alan Lu <alanlu1023@gmail.com>
2019-12-04 11:49:15 -0600

    Tiny linting fixup

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,6 @@ module.exports = {
     ecmaVersion: 2018,
   },
   rules: {
-    'import/no-extraneous-dependencies': ['error', { devDependencies: ['truffle-config.js'] }],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: ['truffle-config.js', 'test/**'] }],
   },
 };
