e5282a4da5fd986779a4b3b0c21d01d81d292bb8
Alan Lu <alanlu1023@gmail.com>
2020-04-27 17:02:06 -0500

    Skip failure test for Safe WalletConnect

--- test/transactions/shouldSupportDifferentTransactions.js
+++ test/transactions/shouldSupportDifferentTransactions.js
@@ -168,7 +168,9 @@ function shouldSupportDifferentTransactions({
       fromWei(await erc20.balanceOf(conditionalTokens.address)).should.equal(1);
     });
 
-    it('by default errors without transacting when single transaction would fail', async () => {
+    (
+      ownerIsRecognizedContract ? it.skip : it
+    )('by default errors without transacting when single transaction would fail', async () => {
       (await multiStep.lastStepFinished(cpk.address)).toNumber().should.equal(0);
       const startingTransactionCount = await getTransactionCount(proxyOwner);
 
