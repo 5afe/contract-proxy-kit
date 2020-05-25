0cf3a40758c9b78cc0bf6606828652064eb17f4a
Germán Martínez <germaga92@gmail.com>
2020-05-25 11:18:06 +0200

    Refactor tests and extract the emulated Safe provider

--- test/index.js
+++ test/index.js
@@ -3,16 +3,19 @@ import { ethers as ethersMaj4 } from 'ethers-4';
 import Web3Maj1Min2 from 'web3-1-2';
 import Web3Maj2Alpha from 'web3-2-alpha';
 import CPK from '../src';
+import CpkTransactionManager from '../src/transactionManagers/CpkTransactionManager';
+import SafeRelayTransactionManager from '../src/transactionManagers/SafeRelayTransactionManager';
 import { zeroAddress } from '../src/utils/constants';
+import EmulatedSafeProvider from './utils/EmulatedSafeProvider';
 import shouldWorkWithWeb3 from './web3/shouldWorkWithWeb3';
 import shouldWorkWithEthers from './ethers/shouldWorkWithEthers';
 
 const web3Versions = [Web3Maj1Min2, Web3Maj2Alpha];
 
 const GnosisSafe = artifacts.require('GnosisSafe');
-const MultiSend = artifacts.require('MultiSend');
-const DefaultCallbackHandler = artifacts.require('DefaultCallbackHandler');
 const ProxyFactory = artifacts.require('ProxyFactory');
+const DefaultCallbackHandler = artifacts.require('DefaultCallbackHandler');
+const MultiSend = artifacts.require('MultiSend');
 
 contract('CPK', ([coinbase, defaultAccount, safeOwner]) => {
   if (!defaultAccount) {
@@ -45,143 +48,15 @@ contract('CPK', ([coinbase, defaultAccount, safeOwner]) => {
     }000000000000000000000000000000000000000000000000000000000000000001`;
     const safe = await GnosisSafe.at(safeAddress);
 
-    const emulatedSafeProvider = {
-      ...web3.currentProvider,
-      wc: {
-        peerMeta: {
-          name: 'Gnosis Safe - Mock',
-        },
-      },
-      send(rpcData, callback) {
-        const {
-          id, jsonrpc, method, params,
-        } = rpcData;
-
-        if (method === 'eth_accounts') {
-          return callback(null, {
-            id, jsonrpc, result: [safeAddress],
-          });
-        }
-
-        if (method === 'eth_sendTransaction') {
-          const [{
-            from, to, gasPrice, value, data, nonce,
-          }] = params;
-
-          if (from.toLowerCase() !== safeAddress.toLowerCase()) {
-            return callback(new Error(`expected to be from safe address ${safeAddress} but got ${from}`));
-          }
-
-          return web3.currentProvider.send({
-            id,
-            jsonrpc,
-            method,
-            params: [{
-              from: safeOwner,
-              to: safeAddress,
-              // Override with 3M as gas limit in this mock provider
-              // as Safe app/gas relayer ultimately has control over
-              // this parameter, so we just set it to some value that
-              // should allow all txs in this test suite to work.
-              gas: web3.utils.toHex(3e6),
-              gasPrice,
-              value,
-              nonce,
-              data: safeMasterCopy.contract.methods.execTransaction(
-                to,
-                value || 0,
-                data,
-                CPK.Call,
-                0,
-                0,
-                0,
-                zeroAddress,
-                zeroAddress,
-                safeSignature,
-              ).encodeABI(),
-            }],
-          }, callback);
-        }
-
-        if (method === 'eth_getTransactionCount') {
-          const [account, block] = params;
-          if (account === safeAddress) {
-            return web3.currentProvider.send({
-              id, jsonrpc, method, params: [safeOwner, block],
-            }, callback);
-          }
-        }
-
-        if (method === 'eth_estimateGas') {
-          const [{
-            from, to, gas, gasPrice, value, data, nonce,
-          }] = params;
-
-          if (from.toLowerCase() === safeAddress.toLowerCase()) {
-            return web3.currentProvider.send({
-              id,
-              jsonrpc,
-              method,
-              params: [{
-                from: safeOwner,
-                to: safeAddress,
-                gas,
-                gasPrice,
-                value,
-                nonce,
-                data: safeMasterCopy.contract.methods.execTransaction(
-                  to,
-                  value || 0,
-                  data,
-                  CPK.Call,
-                  0,
-                  0,
-                  0,
-                  zeroAddress,
-                  zeroAddress,
-                  safeSignature,
-                ).encodeABI(),
-              }],
-            }, callback);
-          }
-        }
-
-        if (method === 'gs_multi_send') {
-          params.forEach((tx) => {
-            if (typeof tx.operation !== 'undefined') {
-              throw new Error('expected operation property to be unset');
-            }
-          });
-
-          const callData = multiSend.contract.methods.multiSend(
-            `0x${params.map((tx) => [
-              web3.eth.abi.encodeParameter('uint8', CPK.Call).slice(-2),
-              web3.eth.abi.encodeParameter('address', tx.to).slice(-40),
-              web3.eth.abi.encodeParameter('uint256', tx.value || 0).slice(-64),
-              web3.eth.abi.encodeParameter('uint256', web3.utils.hexToBytes(tx.data).length).slice(-64),
-              tx.data.replace(/^0x/, ''),
-            ].join('')).join('')}`,
-          ).encodeABI();
-
-          return safe.execTransaction(
-            multiSend.address,
-            0,
-            callData,
-            CPK.DelegateCall,
-            0,
-            0,
-            0,
-            zeroAddress,
-            zeroAddress,
-            safeSignature,
-            { from: safeOwner, gas: web3.utils.toHex(3e6) },
-          ).then(({ tx }) => callback(null, { id, jsonrpc, result: tx }), callback);
-        }
-
-        return web3.currentProvider.send(rpcData, callback);
-      },
-    };
-
+    const emulatedSafeProvider = new EmulatedSafeProvider({
+      safe,
+      safeAddress,
+      safeOwner,
+      safeMasterCopy,
+      multiSend,
+      safeSignature,
+      zeroAddress,
+    });
     gnosisSafeProviderBox[0] = emulatedSafeProvider;
   });
 
@@ -197,8 +72,22 @@ contract('CPK', ([coinbase, defaultAccount, safeOwner]) => {
     await CPK.create({}).should.be.rejectedWith('ethLibAdapter property missing from options');
   });
 
-  web3Versions.forEach((Web3) => {
-    shouldWorkWithWeb3(Web3, defaultAccount, safeOwner, gnosisSafeProviderBox);
+  it('should not produce SafeRelayTransactionManager instances when url not provided', async () => {
+    (() => new SafeRelayTransactionManager({})).should.throw('url property missing from options');
   });
-  shouldWorkWithEthers(ethersMaj4, defaultAccount, safeOwner, gnosisSafeProviderBox);
+
+  describe('with transaction manager CpkTransactionManager', () => {
+    web3Versions.forEach((Web3) => {
+      shouldWorkWithWeb3(Web3, defaultAccount, safeOwner, gnosisSafeProviderBox);
+    });
+    shouldWorkWithEthers(ethersMaj4, defaultAccount, safeOwner, gnosisSafeProviderBox);
+  });
+  
+  describe('with transaction manager SafeRelayTransactionManager', () => {
+    const txManager = new SafeRelayTransactionManager({ url: 'http://localhost:8000' });
+    web3Versions.forEach((Web3) => {
+      shouldWorkWithWeb3(Web3, defaultAccount, safeOwner, gnosisSafeProviderBox, txManager);
+    });
+    shouldWorkWithEthers(ethersMaj4, defaultAccount, safeOwner, gnosisSafeProviderBox, txManager);
+  });
 });
