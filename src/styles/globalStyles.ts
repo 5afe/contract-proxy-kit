d3e577dad0476af534977ceade864cdbdc3ceee0
Germán Martínez <germaga92@gmail.com>
2020-10-06 09:31:56 +0200

    Update global styles

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -6,15 +6,16 @@ const GlobalStyle = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
-    font-size: 14px;
     box-sizing: border-box;
   }
 
-  body {
+  html, body {
     margin: 0;
     padding: 0;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
+  }
+
+  body {
+    padding: 10px;
   }
 
   @font-face {
