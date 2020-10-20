47a0c327e9e703d1c649881bc945b7d78cd1d70c
Germán Martínez <germaga92@gmail.com>
2020-10-20 12:41:03 +0200

    Calculate the contract version manager

--- src/contractManagers/index.ts
+++ src/contractManagers/index.ts
@@ -1,11 +1,13 @@
-import { NetworkConfigEntry } from '../config/networks'
+import { masterCopyAddressVersions, NetworkConfigEntry } from '../config/networks'
 import EthLibAdapter, { Contract } from '../ethLibAdapters/EthLibAdapter'
-import { Address } from '../utils/basicTypes'
 import cpkFactoryAbi from '../abis/CpkFactoryAbi.json'
-import safeAbi from '../abis/SafeAbi.json'
+import safeAbiV111 from '../abis/SafeAbiV1-1-1.json'
+import safeAbiV120 from '../abis/SafeAbiV1-2-0.json'
 import multiSendAbi from '../abis/MultiSendAbi.json'
-import ContractV120Manager from './ContractV120Manager'
 import CommonContractManager from './CommonContractManager'
+import ContractV120Manager from './ContractV120Manager'
+import ContractV111Manager from './ContractV111Manager'
+import { Address } from '../utils/basicTypes'
 
 export interface ContractManagerProps {
   ethLibAdapter: EthLibAdapter
@@ -17,7 +19,7 @@ export interface ContractManagerProps {
 }
 
 class ContractManager {
-  contractVersionManager?: CommonContractManager
+  #contractVersionManager?: CommonContractManager
   #contract?: Contract
   #proxyFactory: Contract
   #masterCopyAddress: Address
@@ -38,29 +40,85 @@ class ContractManager {
   }
 
   async init(opts: ContractManagerProps): Promise<void> {
-    const { ethLibAdapter, network, ownerAccount, saltNonce, isSafeApp, isConnectedToSafe } = opts
+    await this.calculateContractVersionManager(opts)
+  }
+
+  private async calculateContractVersionManager(opts: ContractManagerProps) {
+    const { ethLibAdapter, ownerAccount, saltNonce, isSafeApp, isConnectedToSafe } = opts
+    let proxyAddress
+    let properVersion
+
+    const salt = ethLibAdapter.keccak256(
+      ethLibAdapter.abiEncode(['address', 'uint256'], [ownerAccount, saltNonce])
+    )
+
     if (isSafeApp || isConnectedToSafe) {
-      this.#contract = ethLibAdapter.getContract(safeAbi, ownerAccount)
+      const temporaryContract = ethLibAdapter.getContract(safeAbiV120, ownerAccount)
+      const version: string = await temporaryContract.call('version', [])
+      properVersion = version
     } else {
-      const salt = ethLibAdapter.keccak256(
-        ethLibAdapter.abiEncode(['address', 'uint256'], [ownerAccount, saltNonce])
-      )
-      const initCode = ethLibAdapter.abiEncodePacked(
-        { type: 'bytes', value: await this.#proxyFactory.call('proxyCreationCode', []) },
-        {
-          type: 'bytes',
-          value: ethLibAdapter.abiEncode(['address'], [network.masterCopyAddress])
+      for (const masterCopyVersion of masterCopyAddressVersions) {
+        proxyAddress = await this.calculateProxyAddress(
+          masterCopyVersion.address,
+          salt,
+          ethLibAdapter
+        )
+
+        const codeAtAddress = await ethLibAdapter.getCode(proxyAddress)
+        const isDeployed = codeAtAddress !== '0x'
+        if (isDeployed) {
+          properVersion = masterCopyVersion.version
+          break
         }
-      )
-      const proxyAddress = ethLibAdapter.calcCreate2Address(
-        this.#proxyFactory.address,
-        salt,
-        initCode
-      )
-  
-      this.#contract = ethLibAdapter.getContract(safeAbi, proxyAddress)
+      }
     }
-    this.contractVersionManager = new ContractV120Manager(this.#contract)
+
+    if (!properVersion) {
+      // Last version released
+      properVersion = masterCopyAddressVersions[0].version
+      proxyAddress = await this.calculateProxyAddress(
+        masterCopyAddressVersions[0].address,
+        salt,
+        ethLibAdapter
+      )
+    }
+
+    switch (properVersion) {
+      case '1.2.0':
+        this.#contract = ethLibAdapter.getContract(safeAbiV120, proxyAddress)
+        this.#contractVersionManager = new ContractV120Manager(this.#contract)
+        break
+      case '1.1.1':
+        this.#contract = ethLibAdapter.getContract(safeAbiV111, proxyAddress)
+        this.#contractVersionManager = new ContractV111Manager(this.#contract)
+        break
+      default:
+        throw new Error('CPK Proxy address version is not valid')
+    }
+  }
+
+  private async calculateProxyAddress(
+    masterCopyAddress: Address,
+    salt: string,
+    ethLibAdapter: EthLibAdapter
+  ): Promise<Address> {
+    const initCode = ethLibAdapter.abiEncodePacked(
+      { type: 'bytes', value: await this.#proxyFactory.call('proxyCreationCode', []) },
+      {
+        type: 'bytes',
+        value: ethLibAdapter.abiEncode(['address'], [masterCopyAddress])
+      }
+    )
+    const proxyAddress = ethLibAdapter.calcCreate2Address(
+      this.#proxyFactory.address,
+      salt,
+      initCode
+    )
+    return proxyAddress
+  }
+
+  get contractVersionManager(): CommonContractManager | undefined {
+    return this.#contractVersionManager
   }
 
   get contract(): Contract | undefined {
