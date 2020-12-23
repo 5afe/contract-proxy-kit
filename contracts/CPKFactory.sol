744e621da85ba332762206e8ad8c36cfeea1af3e
Alan Lu <alanlu1023@gmail.com>
2020-12-22 22:26:15 -0600

    Add deleted import from rebase

--- contracts/CPKFactory.sol
+++ contracts/CPKFactory.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.5.0 <0.7.0;
 import { GnosisSafeProxy } from "@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxy.sol";
 import { GnosisSafeProxyFactory } from "@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxyFactory.sol";
 import { GnosisSafe } from "@gnosis.pm/safe-contracts/contracts/GnosisSafe.sol";
+import { ProxyImplSetter } from "./ProxyImplSetter.sol";
 
 contract CPKFactory {
     ProxyImplSetter public proxyImplSetter;
