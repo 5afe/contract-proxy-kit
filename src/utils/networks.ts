dd69af5c2ceb581f94a0905c9f250cdb9a647092
Germán Martínez <germaga92@gmail.com>
2021-02-10 15:39:06 +0100

    Add NetworkNames enum

--- src/utils/networks.ts
+++ src/utils/networks.ts
@@ -1,19 +1,31 @@
+export enum NetworkNames {
+  MAINNET = 'MAINNET',
+  MORDEN = 'MORDEN',
+  ROPSTEN = 'ROPSTEN',
+  RINKEBY = 'RINKEBY',
+  GOERLI = 'GOERLI',
+  KOVAN = 'KOVAN',
+  XDAI = 'XDAI',
+  ENERGY_WEB_CHAIN = 'ENERGY_WEB_CHAIN',
+  VOLTA = 'VOLTA'
+}
+
 type Networks = {
-  [key: string]: number
+  [key in NetworkNames]: number
 }
 
 export const getNetworkIdFromName = (networkName?: string): number | undefined => {
   if (!networkName) return
   const networks: Networks = {
-    mainnet: 1,
-    morden: 2,
-    ropsten: 3,
-    rinkeby: 4,
-    goerli: 5,
-    kovan: 42,
-    xdai: 100,
-    energy_web_chain: 246,
-    volta: 73799
+    MAINNET: 1,
+    MORDEN: 2,
+    ROPSTEN: 3,
+    RINKEBY: 4,
+    GOERLI: 5,
+    KOVAN: 42,
+    XDAI: 100,
+    ENERGY_WEB_CHAIN: 246,
+    VOLTA: 73799
   }
-  return networks[networkName.toLowerCase()]
+  return networks[networkName.toUpperCase() as NetworkNames]
 }
