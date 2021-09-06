bba23f6f4ea43efdad49b2a534762f385832681e
Hamza <hamza.h.hamud@gmail.com>
2021-09-06 17:17:47 +0100

    fix: re-arranged dir

--- packages/cpk-configuration-app/src/utils/balances.ts
+++
@@ -1,9 +0,0 @@
-import BigNumber from 'bignumber.js'
-
-export const formatBalance = (balance: BigNumber | undefined): string => {
-  if (!balance) {
-    return '0 ETH'
-  }
-  const ethDecimals = new BigNumber(10).pow(18)
-  return balance.div(ethDecimals).decimalPlaces(7).toString() + ' ETH'
-}
